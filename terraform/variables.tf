ada5cc545c9cc8ca249f87832e7483ffa1d1bc60
Anthony Vuong <avuong@cachemeoutside.io>
2019-08-08 18:43:22 -0700

    Push for workstation switch tomorrow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,53 @@
-variable "region" {}
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "cred_profile" {
+  default = "default"
+}
 
 variable "src_bucket_name" {}
 
-variable "force_destroy_src_bucket" {}
+variable "src_bucket_region" {}
 
-variable "jp2_bucket_name" {}
-
-variable "force_destroy_jp2_bucket" {}
+variable "function_name" {}
 
 variable "kakadu_lib_layer" {}
 
 variable "kakadu_bin_layer" {}
 
-variable "cred_profile" {}
+# Need to be make this an optional field in the module source code
+variable "force_destroy_src_bucket" {}
+
+variable "jp2_bucket_name" {}
+
+# Need to be make this an optional field in the module source code
+variable "force_destroy_jp2_bucket" {}
 
 variable "project_artifactId" {}
 
 variable "project_version" {}
+
+variable "kakadu_converter_handler" {}
+
+#TODO: this should probably be an optional field
+variable "kakadu_converter_filter_suffix" {}
+
+variable "kakadu_converter_runtime" {}
+
+variable "kakadu_converter_memory_size" {}
+
+variable "kakadu_converter_timeout" {}
+
+variable "kakadu_converter_environment_variables" {}
+
+variable "kakadu_converter_bucket_event" {}
+
+locals {
+  artifact_path = "../target/${var.project_artifactId}-${var.project_version}.jar"
+  lambda_layers = ["${var.kakadu_bin_layer}", "${var.kakadu_lib_layer}"]
+
+  #TODO: figure out a way to map environment variables as map or a list of key value pairs of some sort
+  environment_variables = [ "
+}
+
