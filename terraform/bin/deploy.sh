0dc229beb342001633ab7a24f035a6a12430ce3b
Anthony <vuonga91@gmail.com>
2019-08-13 14:43:49 -0700

    Consolidate deploy script and clean up

--- terraform/bin/deploy.sh
+++ terraform/bin/deploy.sh
@@ -1,11 +1,41 @@
-#! /bin/bash
+#!/bin/bash
+# Configured to use https://apps.terraform.io
 
-#
-# A convenience script to run terraform in this directory. It uses a variables file that has built by Maven.
-#
+TERRAFORM_CLASS_PATH="../target/classes"
+BACKEND_FILE="${TERRAFORM_CLASS_PATH}/backend.hcl"
+TERRAFORM="/opt/terraform/bin/terraform"
+PLAN_FILE="plan.out"
+LOCAL_SECRETS="${TERRAFORM_CLASS_PATH}/terraform.tfvars"
+WORKSPACE="lambda"
 
-# Create a plan based on our current state
-terraform plan -out="plan.out" -var-file="../target/classes/terraform.tfvars"
+if [[ ! -f ${BACKEND_FILE} ]];
+then
+  echo "${BACKEND_FILE} not found"
+  exit
+fi
 
-# Run our plan
-terraform apply -auto-approve -backup=".terraform.tfstate.backup" plan.out
+${TERRAFORM} init \
+  -backend-config="${BACKEND_FILE}"
+
+if [[ -z $(${TERRAFORM} workspace list | grep -i ${WORKSPACE}) ]];
+then
+  ${TERRAFORM} workspace new ${WORKSPACE}
+else
+  ${TERRAFORM} workspace select ${WORKSPACE}
+fi
+
+echo "Working in workspace: $(${TERRAFORM} workspace show)"
+
+
+if [[ -f "${LOCAL_SECRETS}" ]];
+then
+  if [[ $1 == "destroy" ]];
+  then
+    ${TERRAFORM} destroy -var-file="${LOCAL_SECRETS}"
+  else
+    ${TERRAFORM} plan -out ${PLAN_FILE} -var-file="${LOCAL_SECRETS}" 
+  fi
+else
+  ${TERRAFORM} plan -out ${PLAN_FILE}
+fi
+
